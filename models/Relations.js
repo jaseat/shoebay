789166685b919f87118a93a6b8956694d274020e
Anthony Stephen Payne <anthonypayne942@gmail.com>
2018-06-28 19:27:27 -0700

    table relations finished

--- models/Relations.js
+++ models/Relations.js
@@ -1,5 +1,8 @@
+var Privilege = require("./Privilege");
 var Users = require("./Users");
-var Users = require("./Privilege");
+var ArticlesTable = require("./ArticlesTable");
+var ProductTable = require("./ProductTable");
+var CommentsTable = require("./CommentsTable");
 
 Users.associate = function(models) {
   Users.hasMany(models.Privilege, {
@@ -7,10 +10,74 @@ Users.associate = function(models) {
   });
 };
 
-// Privilege.associate = function(models) {
-//   Privilege.belongsTo(models.Users, {
-//     foreignKey: {
-//       allowNull: false,
-//     },
-//   });
-// };
+Privilege.associate = function(models) {
+  Privilege.belongsTo(models.Users, {
+    foreignKey: {
+      allowNull: false,
+    },
+  });
+};
+
+ArticlesTable.associate = function(models) {
+  ArticlesTable.hasMany(models.Users, {
+    onDelete: "cascade",
+  });
+};
+
+Users.associate = function(models) {
+  Users.belongsTo(models.ArticlesTable, {
+    foreignKey: {
+      allowNull: false,
+    },
+  });
+};
+
+ProductTable.associate = function(models) {
+  ProductTable.hasMany(model.Users, {
+    foreignKey: {
+      allowNull: false,
+    },
+  });
+};
+
+CommentsTable.associate = function(models) {
+  CommentsTable.hasMany(models.Users, {
+    onDelete: "cascade",
+  });
+};
+
+Users.associate = function(models) {
+  Users.belongsTo(models.CommentsTable, {
+    foreignKey: {
+      allowNull: false,
+    },
+  });
+};
+
+CommentsTable.associate = function(models) {
+  CommentsTable.hasMany(models.ArticlesTable, {
+    onDelete: "cascade",
+  });
+};
+
+ArticlesTable.associate = function(models) {
+  ArticlesTable.belongsTo(models.CommentsTable, {
+    foreignKey: {
+      allowNull: false,
+    },
+  });
+};
+
+CommentsTable.associate = function(models) {
+  CommentsTable.hasMany(models.ProducTable, {
+    onDelete: "cascade",
+  });
+};
+
+ProducTable.associate = function(models) {
+  ProductTable.belongsTo(models.CommentsTable, {
+    foreignKey: {
+      allowNull: false,
+    },
+  });
+};
--- src/app.js
+++ src/app.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const passport = require('passport');
-// const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 //routes
-const html = require('./routes/html');
-const auth = require('./routes/auth');
+const html = require("./routes/html");
+const auth = require("./routes/auth");
 
 //log for dev
-app.use(logger('dev'));
+app.use(logger("dev"));
 //needed for auth
 app.use(cookieParser());
 //session set up
-app.use(session({ secret: 'should_be_in_env_when_dployed_on_heroku' })); // session secret
+app.use(session({ secret: "should_be_in_env_when_dployed_on_heroku" })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
-app.use(express.static(path.join(__dirname, '/../client/build')));
+app.use(express.static(path.join(__dirname, "/../client/build")));
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   app.use((req, res, next) => {
-    const allowed_header = ['http://localhost:3000'];
+    const allowed_header = ["http://localhost:3000"];
     const origin = req.headers.origin;
     if (allowed_header.indexOf(origin) > -1) {
-      res.header('Access-Control-Allow-Origin', origin);
+      res.header("Access-Control-Allow-Origin", origin);
     }
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, ideaJWT, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, ideaJWT, Accept"
     );
     res.header(
-      'Access-Control-Allow-Methods',
-      'POST, GET, OPTIONS, PUT, DELETE'
+      "Access-Control-Allow-Methods",
+      "POST, GET, OPTIONS, PUT, DELETE"
     );
     next();
   });
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'development') {
   const quote = "test 'testing'";
 }
 
-app.use('/', html);
-app.use('/auth', auth);
+app.use("/", html);
+app.use("/auth", auth);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
