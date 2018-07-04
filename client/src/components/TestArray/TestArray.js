fa0d62a7f97b9fec3f6c37ed80007d8bddfbd327
Anthony Stephen Payne <anthonypayne942@gmail.com>
2018-07-03 19:54:32 -0700

    react virtualize working

---
+++ client/src/components/TestArray/TestArray.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { List } from 'react-virtualized';
+import list from '../List/List';
+
+function rowRenderer({
+  key, // Unique key within array of rows
+  index, // Index of row within collection
+  isScrolling, // The List is currently being scrolled
+  isVisible, // This row is visible within the List (eg it is not an overscanned row)
+  style, // Style object to be applied to row (to position it)
+}) {
+  return (
+    <div key={key} style={style}>
+      {list[index]}
+    </div>
+  );
+}
+
+export default rowRenderer;
