bbf78fe0ef85d30268ece6023ceb1dced2c6cd48
jaseat <jaseat@users.noreply.github.com>
2018-07-07 12:22:50 -0700

    added fetchQuery to front end

---
+++ client/src/utils/api.js
@@ -0,0 +1,24 @@
+/**
+ * Performs a request with any query string.
+ * @param {string} query The query string.
+ * @param {object} variables Object of variables. Keys much match variables in the query string.
+ * @returns Promise containing the object.
+ */
+export const fetchQuery = async (
+  query: string,
+  variables: Object
+): Promise<Object> => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+    headers: {
+      'content-type': 'application/graphql',
+    },
+  };
+  const res = await fetch('api/graphql', options);
+  const data = await res.json();
+  return data;
+};
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,11 +1,28 @@
 const passport = require('../auth');
 
+/**
+ * Get node by id.
+ * @param {string} nodeId Node id string in the format `ta.ble:id`.
+ * @param {object} loaders Node loaders for dataloader
+ * @param {object} db The current database context.
+ * @returns Returns the node specified by nodeId.
+ */
 module.exports.getNodeById = (nodeId, loaders, db) => {
   const { tableName, dbId } = db.splitNodeId(nodeId);
   if (loaders[tableName]) return loaders[tableName].load(dbId);
   else return new Error('Invalid ID');
 };
 
+/**
+ * Creates new user and returns that user.
+ * @param {object} db The current database context.
+ * @param {object} newUser The new user.
+ * @param {string} newUser.firstName First name.
+ * @param {string} newUser.lastName
+ * @param {string} newUser.email
+ * @param {string} newUser.paymentMethod
+ * @param {string} newUser.footmg
+ */
 module.exports.createUser = (
   db,
   { firstName, lastName, email, password, paymentMethod, footImg }
@@ -25,6 +42,13 @@ module.exports.createUser = (
     });
 };
 
+/**
+ * Log-ins user.
+ * @param {string} email User email.
+ * @param {string} password User password.
+ * @param {object} req The current request context.
+ * @returns `User.id` if found, `Unauthorized` otherwise, wrapped in a promise.
+ */
 module.exports.logIn = (email, password, req) => {
   return new Promise((resolve, reject) => {
     req.body = {
@@ -42,6 +66,13 @@ module.exports.logIn = (email, password, req) => {
   });
 };
 
+/**
+ * Performs search based on array of points.
+ * @param {object[]} points Array of points.
+ * @param {number} points.x
+ * @param {number} points.y
+ * @returns List of products ordered by similarity.
+ */
 module.exports.shapeSearch = points => {
   return null;
 };
