c0690307e4306880849b41ddeb06cfbd8998019a
jaseat <jaseat@users.noreply.github.com>
2018-07-07 13:01:21 -0700

    added login to front end

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,8 @@
 /**
  * Performs a request with any query string.
  * @param {string} query The query string.
- * @param {object} variables Object of variables. Keys much match variables in the query string.
- * @returns Promise containing the object.
+ * @param {object} [variables] Object of variables. Keys much match variables in the query string.
+ * @returns Promise containing the data.
  */
 export const fetchQuery = async (
   query: string,
@@ -15,10 +15,24 @@ export const fetchQuery = async (
       variables,
     }),
     headers: {
-      'content-type': 'application/graphql',
+      'content-type': 'application/json',
+      Accept: 'application/json',
     },
   };
   const res = await fetch('api/graphql', options);
   const data = await res.json();
+  return data;
+};
+
+/**
+ * Logs in user.
+ * @param {object} credentials Credentials
+ * @param {string} credentials.email
+ * @param {string} credentials.password
+ * @returns Promise containing user id.
+ */
+export const logIn = async (credentials: Object): Promise<Object> => {
+  const query = `query LogIn($input:LogInInput){logIn(input:$input)}`;
+  const data = await fetchQuery(query, credentials);
   return data;
 };
