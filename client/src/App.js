432d96a6db93cceaffb38cd2910873fbdef755ad
jaseat <jaseat@users.noreply.github.com>
2018-07-07 13:30:38 -0700

    reverted app

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TitleBar from './components/TitleBar';
 import StorePage from './components/StorePage/StorePage';
 
-import { fetchQuery, logIn } from './utils/api';
-
 class App extends Component<{}> {
-  componentDidMount() {
-    const query = `query NodeQuery($input: NodeInput!){
-      node(input:$input){
-        ...on User {
-          id
-          firstName
-        }
-      }
-    }`;
-    const variables = {
-      input: { id: 'user:1' },
-    };
-    fetchQuery(query, variables).then(data => console.log(data));
-    const credentials = {
-      email: 'test@test.com',
-      password: 'test',
-    };
-    logIn(credentials).then(data => console.log(data));
-  }
   render() {
     return (
       <MuiThemeProvider theme={Theme}>
