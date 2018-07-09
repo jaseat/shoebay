9fb2aa08402358e8652d6c6dc4f8f24cae613527
jaseat <jpseatris@ucdavis.edu>
2018-07-08 17:11:08 -0700

    added comma

--- src/db/models/product.js
+++ src/db/models/product.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       width: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
         allowNull: false,
       }
       color: {
